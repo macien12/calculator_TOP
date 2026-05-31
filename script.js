e56e299dbd9bb34c9cc1f75f5d60bb4f0476ed9d
macien12 <160124196+macien12@users.noreply.github.com>
2026-05-31 12:33:46 +0200

    Added html, js, css files
    Some simple concepts for now

---
+++ script.js
@@ -0,0 +1,18 @@
+const firstNumber = "";
+const secondNumber = "";
+
+function addNumbers (a, b) {
+    return a + b;
+}
+
+function subtractNumbers (a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
